5b6dcb65496c7b4de27f113ecc6b91e1ae5df2f9
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2023-06-20 18:57:36 +0200

    icu: Remove this obsolete attempt

--- icu/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    cc::Build::new().file("src/bindings.c").compile("bindings");
-    println!("cargo:rustc-link-lib=dylib=icuuc");
-    println!("cargo:rustc-link-lib=dylib=icui18n");
-}
