119c04801c9410aba6fe1a0b77448b6cd4a82b72
xmppftw <xmppftw@kl.netlib.re>
2023-12-16 12:52:18 +0100

    Fix cargo doc warnings in jid crate

--- jid/src/parts.rs
+++ jid/src/parts.rs
@@ -14,12 +14,14 @@ fn length_check(len: usize, error_empty: Error, error_too_long: Error) -> Result
     }
 }
 
-/// The [`NodePart`] is the optional part before the (optional) `@` in any [`Jid`], whether [`BareJid`] or [`FullJid`].
+/// The [`NodePart`] is the optional part before the (optional) `@` in any [`Jid`][crate::Jid],
+/// whether [`BareJid`][crate::BareJid] or [`FullJid`][crate::FullJid].
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct NodePart(pub(crate) String);
 
 impl NodePart {
-    /// Build a new [`NodePart`] from a string slice. Will fail in case of stringprep validation error.
+    /// Build a new [`NodePart`] from a string slice. Will fail in case of stringprep validation
+    /// error.
     pub fn new(s: &str) -> Result<NodePart, Error> {
         let node = nodeprep(s).map_err(|_| Error::NodePrep)?;
         length_check(node.len(), Error::NodeEmpty, Error::NodeTooLong)?;
@@ -37,12 +39,14 @@ impl fmt::Display for NodePart {
     }
 }
 
-/// The [`DomainPart`] is the part between the (optional) `@` and the (optional) `/` in any [`Jid`], whether [`BareJid`] or [`FullJid`].
+/// The [`DomainPart`] is the part between the (optional) `@` and the (optional) `/` in any
+/// [`Jid`][crate::Jid], whether [`BareJid`][crate::BareJid] or [`FullJid`][crate::FullJid].
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct DomainPart(pub(crate) String);
 
 impl DomainPart {
-    /// Build a new [`DomainPart`] from a string slice. Will fail in case of stringprep validation error.
+    /// Build a new [`DomainPart`] from a string slice. Will fail in case of stringprep validation
+    /// error.
     pub fn new(s: &str) -> Result<DomainPart, Error> {
         let domain = nameprep(s).map_err(|_| Error::NamePrep)?;
         length_check(domain.len(), Error::DomainEmpty, Error::DomainTooLong)?;
@@ -60,12 +64,14 @@ impl fmt::Display for DomainPart {
     }
 }
 
-/// The [`ResourcePart`] is the optional part after the `/` in a [`Jid`]. It is mandatory in [`FullJid`].
+/// The [`ResourcePart`] is the optional part after the `/` in a [`Jid`][crate::Jid]. It is
+/// mandatory in [`FullJid`][crate::FullJid].
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct ResourcePart(pub(crate) String);
 
 impl ResourcePart {
-    /// Build a new [`ResourcePart`] from a string slice. Will fail in case of stringprep validation error.
+    /// Build a new [`ResourcePart`] from a string slice. Will fail in case of stringprep
+    /// validation error.
     pub fn new(s: &str) -> Result<ResourcePart, Error> {
         let resource = resourceprep(s).map_err(|_| Error::ResourcePrep)?;
         length_check(resource.len(), Error::ResourceEmpty, Error::ResourceTooLong)?;
